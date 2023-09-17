655451ca56ae54e34fb5a058a589f685463b6c2b
leobianor <89613674+leobianor@users.noreply.github.com>
2023-09-17 20:25:13 -0300

    Add files via upload

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,8 @@
+export default function Titulo({ children }) {
+    return (
+        <h1 className="font-semibold justify-center text-black text-[27px]">
+        {children}
+        </h1>
+    )
+
+}

787cae0fa848f062315d5ff7c2ee01b3b9ebe91b
leobianor <89613674+leobianor@users.noreply.github.com>
2023-09-30 20:57:00 -0300

    Delete src directory

--- src/components/Titulo.jsx
+++
@@ -1,8 +0,0 @@
-export default function Titulo({ children }) {
-    return (
-        <h1 className="font-semibold justify-center text-black text-[27px]">
-        {children}
-        </h1>
-    )
-
-}
